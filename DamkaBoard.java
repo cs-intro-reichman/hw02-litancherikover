b4088ffc37a570506273fe23cff03e6543742d30
litan cherikover <litan.cherikover@runi.ac.il>
2023-12-30 12:30:47 +0200

    first commit of damkaboard

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -1,8 +1,29 @@
 /**
  *  Gets a command-line argument n (int), and prints an n-by-n damka board.
  */
-public class DamkaBoard {
-	public static void main(String[] args) {
-		System.out.println("hello");
+public class DamkaBoard 
+{
+	public static void main(String[] args) 
+	{
+		//Here, the program gets an argument.
+		int n = Integer.parseInt(args[0]); 
+        for (int i = 0; i < n; i++) 
+		{
+            for (int j = 0; j < n; j++) 
+			{
+                // Even results of i and j will be converted to *
+                if ((i + j) % 2 == 0) 
+				{
+                    System.out.print("*");
+                } 
+				// Odd results of i and j will be converted to empty/white cell
+				else 
+				{
+					//In case the program prints empty squere
+                    System.out.print("  "); 
+                }
+            }
+            System.out.println();
+		}
 	}
 }
