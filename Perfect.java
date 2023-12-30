e4e9dce3dd5f05d8a86cce10c270407ee98da57a
litan cherikover <litan.cherikover@runi.ac.il>
2023-12-30 18:09:06 +0200

    New answer for Perfect

--- Perfect.java
+++ Perfect.java
@@ -5,6 +5,23 @@ public class Perfect
 {
 	public static void main (String[] args) 
 	{
-		//// Put your code here
+		int perfect_number = Integer.parseInt(args[0]);
+		int Total = 0;
+		//Number's proper divisors sum should be equal to the number
+		for ( int i = 1 ; i < perfect_number ; i ++) 
+		{
+			if (perfect_number % i == 0) 
+			{
+				Total = Total + i;
+			}
+		}
+			//The sum of proper divisors should be equal to the number
+			if (perfect_number == Total) 
+			{
+				System.out.print(perfect_number + " is a perfect number");
+			 } else 
+			 {
+			 	System.out.println(perfect_number + " is not a perfect number");
+			 }
 	}
 }
