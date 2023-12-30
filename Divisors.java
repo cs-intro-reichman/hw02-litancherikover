24c9aa2d55e1095ac5b1cb97bb4a5321d5a3b648
litan cherikover <litan.cherikover@runi.ac.il>
2023-12-30 12:56:02 +0200

    new answer for divisor

--- Divisors.java
+++ Divisors.java
@@ -1,8 +1,21 @@
 /**
  *  Gets a command-line argument (int), and prints all the divisors of the given number.
  */
-public class Divisors {
-	public static void main (String[] args) {
-		//// Put your code here
+public class Divisors 
+{
+	public static void main (String[] args) 
+	{
+		//The program get the argument
+		int number = Integer.parseInt(args[0]);
+		//The program start checking from 1
+		int divisor= 1;
+		for (int i = 0 ; i < number ; i++) 
+		{
+			if (number % divisor == 0) 
+			{
+				System.out.println(divisor);
+			}
+			divisor++;
+		}
 	}
 }
